3eecb6258f0250aeb144965c39532243e7e8674e
Shilpi Anand <shilpianand2611@gmail.com>
2023-01-19 15:08:02 +0530

    --v1

--- javascript calculator/index.js
+++ javascript calculator/index.js
@@ -3,7 +3,7 @@ buttons = document.querySelectorAll("button");
 let screenValue = "";
 for (item of buttons){
     item.addEventListener ("click", (e) => {
-        buttonText = e.target.innerText;
+        buttonText = e.target.innerText;    
         if (buttonText == '*'){
             screenValue += buttonText;
             screen.value = screenValue;
